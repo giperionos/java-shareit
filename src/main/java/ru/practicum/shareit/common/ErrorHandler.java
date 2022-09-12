ffda081ad2b91561e7cb2c1a6a2ae57c23a6c150
Alexander Tolstykh <tolstikh-aleksandr@yandex.ru>
2022-09-13 02:07:30 +0300

    style: убрать неиспользуемый импорт.

--- src/main/java/ru/practicum/shareit/common/ErrorHandler.java
+++ src/main/java/ru/practicum/shareit/common/ErrorHandler.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.MissingRequestHeaderException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import ru.practicum.shareit.booking.exceptions.*;
 import ru.practicum.shareit.item.exceptions.CommentForNotExistBookingException;
 import ru.practicum.shareit.item.exceptions.ItemSecurityException;
